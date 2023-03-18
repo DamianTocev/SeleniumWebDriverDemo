493ca74c60bddfba34103295308904cfb92470dd
Damian Tocev <damian.tocev@gmail.com>
2023-03-18 20:10:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumWebDriverDemo
+{
+    public class SeleniumWebDriver
+    {
+        static void Main()
+        {
+            // create browser instance
+            var driver = new ChromeDriver();
+            //var driver = new FirefoxDriver();
+            //var driver = new EdgeDriver();
+
+            // navigate to Wikipedia
+            driver.Url = "https://www.wikipedia.org/";
+            //driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+
+            // get browser title
+            var pageTitle = driver.Title;
+
+            Console.WriteLine("The page title is " + pageTitle);
+
+            if (pageTitle == "Wikipedia")
+            {
+                Console.WriteLine("TEST PASS");
+            } else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+            // close browser
+            driver.Quit();
+        }
+    }
+}
